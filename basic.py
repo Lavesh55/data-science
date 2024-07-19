f01847da583fe0b876997280c19d412181eebbb0
lavesh55 <laveshgujar71@gmail.com>
2024-07-19 10:22:53 +0530

    hello python

---
+++ basic.py
@@ -0,0 +1,6 @@
+a=9
+b=4
+print("sum=",a-b)
+name =input("enter your name")
+print(name)
+
---
+++ my.py
@@ -0,0 +1,26 @@
+"""age =int (input("your age"))
+if age >=18:
+    {
+print("you are adult")
+    }
+else:{
+print("you are not adult")}"""
+
+'''for i in range(5):
+    print(i)'''
+'''
+num =1
+while num<=5:
+    
+    print(num)
+    num +=1'''
+
+'''n=23012
+while n<=230120:
+    print(n)
+    n+=23012'''
+#complex=a + b(3+4j)
+# a=3+3
+# b=3+5j
+# print(a+b)    
+
